1873bb7b3b279f230e611d271a8f198b4d9ee2b7
Laurence Cowton <laurence.c@asidatascience.com>
2019-05-09 12:11:53 +0000

    add path variable to executer function

--- faculty_parallel/bin/run_jobs.py
+++ faculty_parallel/bin/run_jobs.py
@@ -2,25 +2,23 @@ import cloudpickle
 import os
 import sys
 import click
-from faculty_parallel.utils import most_recent_job_dirs
 
 
 @click.command()
+@click.argument("path")
 @click.argument("n")
-def execute_func(n):
+def execute_on_worker(path, n):
     """
     Loads function and arguments from binary serialisation, executes function
     and pickles the output. Output of function is generic. 
     Parameters
     ----------
     path: str
-        path to folders to save functions and save output
+        path to directory to load function and arguments and save output
     n: int 
-        job number
+        worker number
     
     """
-    path = most_recent_job_dirs(os.environ["JOB_PATH"])[0]
-
     with open(os.path.join(path, "func/func.txt"), "rb") as f:
         func = cloudpickle.load(f)
     with open(os.path.join(path, f"func/args_{n}.txt"), "rb") as f:
